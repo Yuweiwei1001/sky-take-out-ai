96ed0dd9cc8bd40c28b41e8e1d7d17c11c947644
Yuwei <1748546309@qq.com>
2025-03-28 15:46:31 +0800

    导出运营数据报表功能

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 /**
@@ -70,5 +71,11 @@ public class ReportController {
             @DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate end){
         log.info("销量排名top10:{}~{}",begin,end);
         return Result.success(reportService.getSalesTop10(begin,end));
+    }
+
+    @GetMapping("/export")
+    @ApiOperation("导出Excel报表数据")
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessData(response);
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -5,14 +5,20 @@ import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
-import com.sky.vo.OrderReportVO;
-import com.sky.vo.SalesTop10ReportVO;
-import com.sky.vo.TurnoverReportVO;
-import com.sky.vo.UserReportVO;
+import com.sky.service.WorkspaceService;
+import com.sky.vo.*;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -34,6 +40,9 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private WorkspaceService workspaceService;
 
     /**
      * @description 统计指定时间区间内的营业额数据
@@ -184,5 +193,76 @@ public class ReportServiceImpl implements ReportService {
                 .numberList(numberListStr)
                 .nameList(nameListStr)
                 .build();
+    }
+
+    /**
+     * 导出报表数据
+     * @param response
+     */
+    @Override
+    public void exportBusinessData(HttpServletResponse response) {
+        // 1.查询数据库，获取营业数据
+        LocalDate beginDate = LocalDate.now().minusDays(30);
+        LocalDate endDate = LocalDate.now().minusDays(1);
+        LocalDateTime beginTime = LocalDateTime.of(beginDate, LocalTime.MIN);
+        LocalDateTime endTime = LocalDateTime.of(endDate, LocalTime.MAX);
+        BusinessDataVO businessData = workspaceService.getBusinessData(beginTime, endTime);
+        // 2.将数据写入excel
+        // 读取模板文件
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("template/business-data.xlsx");
+
+        try {
+            //基于模板文件创建一个新的excel文件
+            XSSFWorkbook excel = new XSSFWorkbook(inputStream);
+
+            //获取sheet
+            XSSFSheet sheet = excel.getSheet("Sheet1");
+
+            // 获取行
+            XSSFRow row1 = sheet.getRow(1);
+            // 填充数据 - 时间
+            row1.getCell(1).setCellValue("时间" + beginDate + "至" + endDate);
+            XSSFRow row3 = sheet.getRow(3);
+            // 填充数据 - 营业额
+            row3.getCell(2).setCellValue(businessData.getTurnover());
+            // 填充数据 - 订单完成率
+            row3.getCell(4).setCellValue(businessData.getOrderCompletionRate());
+            // 填充数据 - 新增用户数
+            row3.getCell(6).setCellValue(businessData.getNewUsers());
+            XSSFRow row4 = sheet.getRow(4);
+            // 填充数据 - 有效订单
+            row4.getCell(2).setCellValue(businessData.getValidOrderCount());
+            // 填充数据 - 平均客单价
+            row4.getCell(4).setCellValue(businessData.getUnitPrice());
+
+            // 填充明细数据
+           for (int i = 0; i < 30 ; i++){
+                LocalDate date = beginDate.plusDays(i);
+                //查询某一天的营业额
+                BusinessDataVO businessDataOneDay = workspaceService.getBusinessData(LocalDateTime.of(date, LocalTime.MIN), LocalDateTime.of(date, LocalTime.MAX));
+
+                //获取某一行
+                // 填充数据 - 明细日期
+                XSSFRow row = sheet.getRow(7 + i);
+                row.getCell(1).setCellValue(date.toString());
+                // 填充数据 - 营业额
+                row.getCell(2).setCellValue(businessDataOneDay.getTurnover());
+                // 填充数据 - 有效订单
+                row.getCell(3).setCellValue(businessDataOneDay.getValidOrderCount());
+                // 填充数据 - 订单完成率
+                row.getCell(4).setCellValue(businessDataOneDay.getOrderCompletionRate());
+                // 填充数据 - 平均客单价
+                row.getCell(5).setCellValue(businessDataOneDay.getUnitPrice());
+                // 填充数据 - 新增用户数
+                row.getCell(6).setCellValue(businessDataOneDay.getNewUsers());
+           }
+
+            // 3.通过输出流，将excel下载到客户端浏览器
+            OutputStream outputStream = response.getOutputStream();
+            excel.write(outputStream);
+        } catch (IOException e) {
+            throw new RuntimeException("导出报表失败");
+        }
+
     }
 }
